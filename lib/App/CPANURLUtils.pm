2d1bd6ccbe7ef95dc63cc04b02e3a87c2c409e04
perlancar (@pc-home) <perlancar@gmail.com>
2016-03-07 11:37:21 +0700

    Initial commit

---
+++ lib/App/CPANURLUtils.pm
@@ -0,0 +1,109 @@
+package App::CPANURLUtils;
+
+# DATE
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+
+our %SPEC;
+
+our %common_args = (
+    urls => {
+        'x.name.is_plural' => 1,
+        'x.name.singular' => 'url',
+        schema => ['array*', of=>'str*', min_len=>1],
+        req => 1,
+        pos => 0,
+        greedy => 1,
+        cmdline_src => 'stdin_or_args',
+    },
+);
+
+$SPEC{url2cpaninfo} = {
+    v => 1.1,
+    summary => 'Extract CPAN information from URL(s)',
+    args => {
+        %common_args,
+    },
+    result_naked => 1,
+};
+sub url2cpaninfo {
+    require CPAN::Info::From::URL;
+    my %args = @_;
+    [map {CPAN::Info::From::URL::extract_cpan_info_from_url($_)}
+         @{ $args{urls} }];
+}
+
+$SPEC{url2cpanmod} = {
+    v => 1.1,
+    summary => 'Extract CPAN module from URL(s)',
+    args => {
+        %common_args,
+    },
+    result_naked => 1,
+};
+sub url2cpanmod {
+    require CPAN::Module::From::URL;
+    my %args = @_;
+    [map {CPAN::Module::From::URL::extract_cpan_module_from_url($_)}
+         @{ $args{urls} }];
+}
+
+$SPEC{url2cpandist} = {
+    v => 1.1,
+    summary => 'Extract CPAN distribution from URL(s)',
+    args => {
+        %common_args,
+    },
+    result_naked => 1,
+};
+sub url2cpandist {
+    require CPAN::Dist::From::URL;
+    my %args = @_;
+    [map {CPAN::Dist::From::URL::extract_cpan_dist_from_url($_)}
+         @{ $args{urls} }];
+}
+
+$SPEC{url2cpanauthor} = {
+    v => 1.1,
+    summary => 'Extract CPAN author from URL(s)',
+    args => {
+        %common_args,
+    },
+    result_naked => 1,
+};
+sub url2cpanauthor {
+    require CPAN::Author::From::URL;
+    my %args = @_;
+    [map {CPAN::Author::From::URL::extract_cpan_author_from_url($_)}
+         @{ $args{urls} }];
+}
+
+$SPEC{url2cpanrel} = {
+    v => 1.1,
+    summary => 'Extract CPAN release (tarball) name from URL(s)',
+    args => {
+        %common_args,
+    },
+    result_naked => 1,
+};
+sub url2cpanrel {
+    require CPAN::Release::From::URL;
+    my %args = @_;
+    [map {CPAN::Release::From::URL::extract_cpan_release_from_url($_)}
+         @{ $args{urls} }];
+}
+
+1;
+
+# ABSTRACT: Utilities related to CPAN URLs
+
+=head1 DESCRIPTION
+
+This distribution contains the following utilities:
+
+# INSERT_EXECS_LISTS
+
+=cut
